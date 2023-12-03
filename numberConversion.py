1402e430f2f51bc0b92787b7deda175a74cafbd7
Konstantinos Adamis <137218524+KonstantinosAdamis@users.noreply.github.com>
2023-12-03 21:39:30 +0200

    Add files via upload

---
+++ numberConversion.py
@@ -0,0 +1,35 @@
+b_num = input("Give a binary number: ")
+
+s = 0
+
+j = len(b_num) - 1
+
+for i in range(len(b_num)):
+
+	s += int(b_num[i])*(2**j)
+
+	j -= 1
+
+print(s)
+
+d_num = int(input("Give a decimal number: "))
+
+def binary(d_num):
+
+	digit_arr = []
+
+	while d_num != 0:
+
+		if (d_num % 2) == 0:
+
+			digit_arr.append(0)
+
+		else:
+
+			digit_arr.append(1)
+
+		d_num //= 2
+
+	return (list(reversed(digit_arr)))
+
+print(binary(d_num))
